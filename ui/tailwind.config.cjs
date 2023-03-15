d37f63a722121b594923d317928eecc3e681af02
Roberto Espinoza <50055949+Robertorc83@users.noreply.github.com>
2023-03-15 15:33:02 -0500

    Add tailwind custom fonts, styles and animation

--- ui/tailwind.config.cjs
+++ ui/tailwind.config.cjs
@@ -4,7 +4,31 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'main': ['Quicksand', 'sans-serif'],
+        'oxygen':['Oxygen', 'sans-serif']
+      },
+      colors:{
+        clay: '#faf7fa',
+        dark_blue: '#152f49'
+      },
+      animation: {
+        text: 'text 5s ease infinite',
+      },
+      keyframes: {
+        text: {
+          '0%, 100%': {
+            'background-size': '200% 200%',
+            'background-position': 'left center',
+          },
+          '50%': {
+            'background-size': '200% 200%',
+            'background-position': 'right center',
+          },
+        },
+      },
+    },
   },
   plugins: [],
 }
