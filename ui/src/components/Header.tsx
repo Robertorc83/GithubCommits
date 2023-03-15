69f0d55a6d91a03a908b64ca9c35d49362c2ee40
Roberto Espinoza <50055949+Robertorc83@users.noreply.github.com>
2023-03-15 15:39:23 -0500

    Add header and footer

---
+++ ui/src/components/Header.tsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <div className="py-2 bg-dark_blue">
+        <h1
+            className=" font-main font-bold animate-text bg-gradient-to-r from-teal-100 via-teal-300 to-teal-400 bg-clip-text text-transparent text-[35px] ml-10 break-words"
+        >
+            Github Commits
+        </h1> 
+    </div>
+  )
+}
